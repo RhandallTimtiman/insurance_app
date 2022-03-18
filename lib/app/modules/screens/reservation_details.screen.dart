0b8d482acecaf4d2974f36b80a425fd9acf3c8f5
Rhandall Timtiman <rhandalltimtiman@XLOGs-iMac-2.local>
2022-03-18 15:56:42 +0800

    Added Design in Details Page

--- lib/app/modules/screens/reservation_details.screen.dart
+++ lib/app/modules/screens/reservation_details.screen.dart
@@ -12,10 +12,12 @@ class ReservationDetailsScreen extends StatefulWidget {
 class _ReservationDetailsScreenState extends State<ReservationDetailsScreen> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: const MainAppBar(
         title: Text(
-          'Reservation Details',
+          '',
           style: TextStyle(
             fontSize: 15,
             fontWeight: FontWeight.w500,
@@ -23,7 +25,92 @@ class _ReservationDetailsScreenState extends State<ReservationDetailsScreen> {
           ),
         ),
       ),
-      body: Container(),
+      backgroundColor: const Color.fromRGBO(
+        41,
+        50,
+        64,
+        1,
+      ),
+      body: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 15,
+                horizontal: 20,
+              ),
+              height: 80,
+              color: const Color.fromRGBO(
+                41,
+                50,
+                64,
+                1,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    'XLOG-12314',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      letterSpacing: 1,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Status: New',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 30,
+                  horizontal: 30,
+                ),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(
+                      70,
+                    ),
+                  ),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        color: Colors.pink,
+                        height: 400,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        color: Colors.pink,
+                        height: 400,
+                      ),
+                      Container(
+                        color: Colors.pink,
+                        height: 400,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
