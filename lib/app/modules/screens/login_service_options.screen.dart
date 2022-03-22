c9a143b00f18d704b89cda70848302118f88004c
ced <cedrick.sestoso@myxlog.com>
2022-03-22 11:11:35 +0800

    redesign service option tile

--- lib/app/modules/screens/login_service_options.screen.dart
+++ lib/app/modules/screens/login_service_options.screen.dart
@@ -47,73 +47,6 @@ class _LoginServiceOptionScreenState extends State<LoginServiceOptionScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    // const Text(
-                    //   'LOGIN AS',
-                    //   style: TextStyle(
-                    //     fontSize: 24,
-                    //     fontWeight: FontWeight.bold,
-                    //     color: Color.fromRGBO(
-                    //       2,
-                    //       39,
-                    //       108,
-                    //       1,
-                    //     ),
-                    //   ),
-                    // ),
-                    // const SizedBox(
-                    //   height: 5,
-                    // ),
-                    // Row(
-                    //   crossAxisAlignment: CrossAxisAlignment.start,
-                    //   children: const [
-                    //     Expanded(
-                    //       flex: 1,
-                    //       child: Divider(
-                    //         thickness: 2,
-                    //         color: Color.fromRGBO(
-                    //           2,
-                    //           39,
-                    //           108,
-                    //           1,
-                    //         ),
-                    //       ),
-                    //     ),
-                    //     SizedBox(
-                    //       width: 20,
-                    //     ),
-                    //     Text(
-                    //       'Services',
-                    //       style: TextStyle(
-                    //         fontSize: 16,
-                    //         fontWeight: FontWeight.bold,
-                    //         color: Color.fromRGBO(
-                    //           2,
-                    //           39,
-                    //           108,
-                    //           1,
-                    //         ),
-                    //       ),
-                    //     ),
-                    //     SizedBox(
-                    //       width: 20,
-                    //     ),
-                    //     Expanded(
-                    //       flex: 1,
-                    //       child: Divider(
-                    //         thickness: 2,
-                    //         color: Color.fromRGBO(
-                    //           2,
-                    //           39,
-                    //           108,
-                    //           1,
-                    //         ),
-                    //       ),
-                    //     )
-                    //   ],
-                    // ),
-                    // const SizedBox(
-                    //   height: 20,
-                    // ),
                     GetBuilder<ServiceController>(
                       builder: (_) {
                         return SizedBox(
@@ -132,18 +65,6 @@ class _LoginServiceOptionScreenState extends State<LoginServiceOptionScreen> {
                         );
                       },
                     ),
-                    // Column(
-                    //   children: services.map((e) {
-                    //     return Column(
-                    //       children: [
-                    //         ServiceOptionTile(serviceDetails: e),
-                    //         const SizedBox(
-                    //           height: 10,
-                    //         )
-                    //       ],
-                    //     );
-                    //   }).toList(),
-                    // ),
                   ],
                 ),
               ),
