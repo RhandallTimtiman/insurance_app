dace52224c8264d02914983a34daed8079771309
ced <cedrick.sestoso@myxlog.com>
2022-03-22 11:03:33 +0800

    redesign service option tile

--- lib/app/modules/screens/login_service_options.screen.dart
+++ lib/app/modules/screens/login_service_options.screen.dart
@@ -47,116 +47,103 @@ class _LoginServiceOptionScreenState extends State<LoginServiceOptionScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Container(
-                      width: size.width * 0.97,
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Text(
-                            'LOGIN AS',
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromRGBO(
-                                2,
-                                39,
-                                108,
-                                1,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              Expanded(
-                                flex: 1,
-                                child: Divider(
-                                  thickness: 2,
-                                  color: Color.fromRGBO(
-                                    2,
-                                    39,
-                                    108,
-                                    1,
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                width: 20,
-                              ),
-                              Text(
-                                'Services',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color.fromRGBO(
-                                    2,
-                                    39,
-                                    108,
-                                    1,
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                width: 20,
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: Divider(
-                                  thickness: 2,
-                                  color: Color.fromRGBO(
-                                    2,
-                                    39,
-                                    108,
-                                    1,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          GetBuilder<ServiceController>(
-                            builder: (_) {
+                    // const Text(
+                    //   'LOGIN AS',
+                    //   style: TextStyle(
+                    //     fontSize: 24,
+                    //     fontWeight: FontWeight.bold,
+                    //     color: Color.fromRGBO(
+                    //       2,
+                    //       39,
+                    //       108,
+                    //       1,
+                    //     ),
+                    //   ),
+                    // ),
+                    // const SizedBox(
+                    //   height: 5,
+                    // ),
+                    // Row(
+                    //   crossAxisAlignment: CrossAxisAlignment.start,
+                    //   children: const [
+                    //     Expanded(
+                    //       flex: 1,
+                    //       child: Divider(
+                    //         thickness: 2,
+                    //         color: Color.fromRGBO(
+                    //           2,
+                    //           39,
+                    //           108,
+                    //           1,
+                    //         ),
+                    //       ),
+                    //     ),
+                    //     SizedBox(
+                    //       width: 20,
+                    //     ),
+                    //     Text(
+                    //       'Services',
+                    //       style: TextStyle(
+                    //         fontSize: 16,
+                    //         fontWeight: FontWeight.bold,
+                    //         color: Color.fromRGBO(
+                    //           2,
+                    //           39,
+                    //           108,
+                    //           1,
+                    //         ),
+                    //       ),
+                    //     ),
+                    //     SizedBox(
+                    //       width: 20,
+                    //     ),
+                    //     Expanded(
+                    //       flex: 1,
+                    //       child: Divider(
+                    //         thickness: 2,
+                    //         color: Color.fromRGBO(
+                    //           2,
+                    //           39,
+                    //           108,
+                    //           1,
+                    //         ),
+                    //       ),
+                    //     )
+                    //   ],
+                    // ),
+                    // const SizedBox(
+                    //   height: 20,
+                    // ),
+                    GetBuilder<ServiceController>(
+                      builder: (_) {
+                        return SizedBox(
+                          width: size.width * 0.5,
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: _.services.length,
+                            itemBuilder: ((ctx, index) {
                               return Center(
-                                child: ListView.builder(
-                                  shrinkWrap: true,
-                                  itemCount: _.services.length,
-                                  itemBuilder: ((ctx, index) {
-                                    return Center(
-                                      child: ServiceOptionTile(
-                                        service: _.services[index],
-                                      ),
-                                    );
-                                  }),
+                                child: ServiceOptionTile(
+                                  service: _.services[index],
                                 ),
                               );
-                            },
+                            }),
                           ),
-                          // Column(
-                          //   children: services.map((e) {
-                          //     return Column(
-                          //       children: [
-                          //         ServiceOptionTile(serviceDetails: e),
-                          //         const SizedBox(
-                          //           height: 10,
-                          //         )
-                          //       ],
-                          //     );
-                          //   }).toList(),
-                          // ),
-                        ],
-                      ),
+                        );
+                      },
                     ),
+                    // Column(
+                    //   children: services.map((e) {
+                    //     return Column(
+                    //       children: [
+                    //         ServiceOptionTile(serviceDetails: e),
+                    //         const SizedBox(
+                    //           height: 10,
+                    //         )
+                    //       ],
+                    //     );
+                    //   }).toList(),
+                    // ),
                   ],
                 ),
               ),
